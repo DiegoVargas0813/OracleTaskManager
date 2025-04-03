5ace0c3187228eca76523c5c90913ca9cfc43af6
DiegoVargas0813 <A01635782@tec.mx>
2025-04-03 02:55:09 -0600

    Return login

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/security/WebSecurityConfiguration.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/security/WebSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.springboot.MyTodoList.security;
+/* package com.springboot.MyTodoList.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,8 +18,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 }
 
+ */
 
-/*
 package com.springboot.MyTodoList.security;
 
 import org.springframework.context.annotation.Bean;
@@ -39,4 +39,5 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 formLogin().and().logout().permitAll();
     }
 }
-*/
+
+
