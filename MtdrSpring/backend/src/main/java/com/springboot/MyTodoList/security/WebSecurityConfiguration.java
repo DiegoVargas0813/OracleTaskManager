00d0950b52484e7ad956ba668487c77a2630dbcd
DiegoVargas0813 <A01635782@tec.mx>
2025-04-03 14:21:03 -0600

    Changed web security

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/security/WebSecurityConfiguration.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/security/WebSecurityConfiguration.java
@@ -34,9 +34,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable();
-        httpSecurity.authorizeRequests().anyRequest().authenticated().and().
-                formLogin().and().logout().permitAll();
+        httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .authorizeRequests(requests -> requests
+        .antMatchers("/**").permitAll());
     }
 }
 
