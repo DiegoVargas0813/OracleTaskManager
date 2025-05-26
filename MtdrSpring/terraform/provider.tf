028fdf89fb9110c61536b0012466b462b308c4c5
DiegoVargas0813 <A01635782@tec.mx>
2025-05-26 10:08:42 -0600

    Provider.tf changes

--- MtdrSpring/terraform/provider.tf
+++ MtdrSpring/terraform/provider.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
-provider "oci"{
-  region = var.ociRegionIdentifier
+provider "oci" {
+  alias        = "home_region"
+  tenancy_ocid = var.tenancy_ocid
+  region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
+}
+
+provider "oci" {
+  alias        = "current_region"
+  tenancy_ocid = var.tenancy_ocid
+  region       = var.region
 }
